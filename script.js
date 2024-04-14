1e733ab0913d56c057a38e7e385496964178142e
kaveeshaTheekshanee <161103393+kaveeshaTheekshanee@users.noreply.github.com>
2024-04-14 14:56:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -44,6 +44,19 @@ window.onscroll = function () {
 
     prevScrollPos = currentScrollPos;
 };
+
+let next = document.querySelector('.next')
+let prev = document.querySelector('.prev')
+
+next.addEventListener('click', function(){
+    let items = document.querySelectorAll('.item')
+    document.querySelector('.slide').appendChild(items[0])
+})
+
+prev.addEventListener('click', function(){
+    let items = document.querySelectorAll('.item')
+    document.querySelector('.slide').prepend(items[items.length - 1]) // here the length of items = 6
+})
 document.addEventListener("DOMContentLoaded", function () {
     var goToTopBtn = document.getElementById("goToTopBtn");
 
@@ -64,50 +77,8 @@ function goToTop() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
 }
-let circle = document.querySelector('.circle');
-let slider = document.querySelector('.slider');
-let list = document.querySelector('.list');
-let prev = document.getElementById('prev');
-let next = document.getElementById('next');
-let items = document.querySelectorAll('.list .item');
-let count = items.length;
-let active = 1;
-let leftTransform = 0;
-let width_item = items[active].offsetWidth;
 
-next.onclick = () => {
-    active = active >= count - 1 ? count - 1 : active + 1;
-    runCarousel();
-}
-prev.onclick = () => {
-    active = active <= 0 ? active : active - 1;
-    runCarousel();
-}
-function runCarousel() {
-    prev.style.display = (active == 0) ? 'none' : 'block';
-    next.style.display = (active == count - 1) ? 'none' : 'block';
-
-
-    let old_active = document.querySelector('.item.active');
-    if(old_active) old_active.classList.remove('active');
-    items[active].classList.add('active');
-
-     leftTransform = width_item * (active - 1) * -1;
-    list.style.transform = `translateX(${leftTransform}px)`;
-}
-runCarousel();
-
-
-// Set Text on a Circle
-let textCircle = circle.innerText.split('');
-circle.innerText = '';
-textCircle.forEach((value, key) => {
-    let newSpan =  document.createElement("span");
-    newSpan.innerText = value;
-    let rotateThisSpan = (360 / textCircle.length) * (key + 1);
-    newSpan.style.setProperty('--rotate', rotateThisSpan + 'deg');
-    circle.appendChild(newSpan); 
-});
+/*shop*/
 let nextBtn = document.querySelector('.next')
 let prevBtn = document.querySelector('.prev')
 
